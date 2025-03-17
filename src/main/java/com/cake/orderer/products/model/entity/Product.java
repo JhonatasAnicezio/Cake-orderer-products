b7baa32e434e7dd39f12b2965af732328eeff5a9
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2025-03-16 21:53:43 -0300

    feat: create entity and repository

--- src/main/java/com/cake/orderer/products/model/entity/Product.java
+++ src/main/java/com/cake/orderer/products/model/entity/Product.java
@@ -1,5 +1,28 @@
 package com.cake.orderer.products.model.entity;
 
-public class Product
-{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
+public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+  private List<String> size;
+  private String name;
+  private List<Float> price;
+  private String description;
+  private String  nutritionTable;
+  private String content;
 }
--- src/main/java/com/cake/orderer/products/model/repository/ProductRepository.java
+++ src/main/java/com/cake/orderer/products/model/repository/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.cake.orderer.products.model.repository;
 
-public class ProductRepository
-{
-}
+import com.cake.orderer.products.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, String> { }
